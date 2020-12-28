b982a49d92aae8092925d10c945e0295fae31e53
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-12-28 15:06:50 +0530

    updated Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import Form from './Form/Form'
-// import useStyles from './styles;
+import List from './List/List';
+import useStyles from './styles';
 
 const ExpenseTracker = () => {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
     <Card>
       <CardHeader title = 'Expense Tracker' />
